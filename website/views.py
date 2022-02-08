ba18929c3f416a0acfc763b72ab39366a1fe6962
Odiljon Gaybullaev <odiljongaybullayev@mail.ru>
2022-02-08 18:31:23 +0500

    work

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
+import urllib
 from flask import render_template
 from website import app
+import json
 
 
 @app.route('/')
@@ -8,9 +10,11 @@ def home_page():
     return render_template('home.html')
 
 
-@app.route('/intro')
-def intro_page():
-    return render_template('intro.html')
-
+@app.route("/ExchangeRates")
+def GetExchangeRates():
+    url = "http://172.20.0.31:4444/Api/C0mplexApi/GetExchangeRates"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    return render_template('ExchangeRates.html', data=json.loads(data), index=0)
 
 
