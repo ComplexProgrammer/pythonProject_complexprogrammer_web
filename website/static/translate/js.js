0089f69d8a6280ca346b0373e86131a9ea0ac1f5
Xalilov Davronbek Qo'chqor o'g'li <mrdavrikdavron@gmail.com>
2022-06-27 15:54:42 +0500

    27.06.2022

--- website/static/translate/js.js
+++ website/static/translate/js.js
@@ -15,7 +15,27 @@ var app = angular.module("App", []);
             alertify.error("Xatolik yuz berdi");
             console.log("error in GetTranslateLanguages -> ", error);
     });
+    $scope.speech=function(text){
+        $http({
+            method: 'POST',
+            url: "/TextToSpeech?text="+text,
+            }).then(function (d) {
+                console.log(d.data.data);
+            }, function (error) {
+                console.log("error in TextToSpeech -> ", error);
+        });
+    }
+    $scope.replace=function(){
+        $scope.src1=$scope.src;
+        $scope.src=$scope.dest;
+        $scope.dest=$scope.src1;
+        $scope.text1=$scope.text;
+        $scope.text=$scope.result;
+        $scope.result=$scope.text1;
+
+    }
     $scope.getResult=function() {
+        $scope.result='';
         document.getElementById('conn').style.visibility = "visible";
         $http({
             method: 'POST',
@@ -25,14 +45,6 @@ var app = angular.module("App", []);
             console.log(d.data.data);
             $scope.Data = d.data.data;
             $scope.result=$scope.Data;
-            $http({
-                method: 'POST',
-                url: "/TextToSpeech?text="+$scope.result,
-                }).then(function (d) {
-                    console.log(d.data.data);
-                }, function (error) {
-                    console.log("error in TextToSpeech -> ", error);
-            });
             alertify.success("Natija tayyor");
             document.getElementById('conn').style.visibility = "hidden";
         }, function (error) {
