8fed4147be2ea426cb0bdc8b4dfd46eebf4a1513
odilj <gaybullayevodiljon@gmail.com>
2022-09-29 23:34:39 +0500

    home

--- test.py
+++ test.py
@@ -1,11 +1,40 @@
-import googletrans
-from googletrans import Translator
+import os
+import instaloader
 
-translator = Translator()
-result = translator.translate('Mitä sinä teet')
+from instaloader import Instaloader, Profile
+print("start...")
+L = Instaloader()
+PROFILE = "it_programmerr"
+profile = Profile.from_username(L.context, PROFILE)
 
-print(result.src)
-print(result.dest)
-print(result.origin)
-print(result.text)
-print(result.pronunciation)
+posts_sorted_by_likes = sorted(profile.get_posts(), key=lambda post: post.likes,reverse=True)
+
+try:
+    for post in posts_sorted_by_likes:
+        if post.is_video:
+            print(post)
+            print(post.url)
+            L.download_post(post, PROFILE)
+except IndexError:
+    print("You have no saved posts yet.")
+print("end...")
+
+
+def save_insta_collection():
+    username = "complexprogrammer"
+    loader = instaloader.Instaloader()
+    # loader.load_session_from_file(username)
+    profile = instaloader.Profile.from_username(loader.context, username)
+
+    post_list = []
+    try:
+        for saved_posts in profile.get_saved_posts():
+            post_list.append(saved_posts)
+        loader.download_post(post_list[0], 'mysavedcollection')
+
+    except IndexError:
+        print("You have no saved posts yet.")
+
+    save_insta_collection()  # recursion
+
+
